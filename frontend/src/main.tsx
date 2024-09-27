46d61a61648c4d06484dc075e1bb61f51977e0a2
Pasha Okuneu <paul.okunev00@gmail.com>
2024-09-27 13:25:32 +0200

    Disable mini app window vertical scrolling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './globalStyle';
 import 'react-loading-skeleton/dist/skeleton.css';
-import { telegram_expand, telegram_ready } from './Telegram/utils.ts';
+import { telegram_disableVerticalSwipes, telegram_expand, telegram_ready } from './Telegram/utils.ts';
 
 telegram_ready();
 telegram_expand();
+telegram_disableVerticalSwipes();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
