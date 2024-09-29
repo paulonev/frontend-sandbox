e8beda2730a056b1458917d3de3e51b9b4699b5f
alexander <sashamoskalik@gmail.com>
2024-09-30 00:13:22 +0300

    added webp64 in db

--- backend/src/main/java/org/example/db/crypto/cryptocurrencies/CryptoCurrenciesDao.kt
+++ backend/src/main/java/org/example/db/crypto/cryptocurrencies/CryptoCurrenciesDao.kt
@@ -2,9 +2,10 @@ package org.example.db.crypto.cryptocurrencies
 
 object CryptoCurrenciesDao {
 
-    fun create(name: String, ticker: String) = CryptoCurrenciesEntity.new {
+    fun create(name: String, ticker: String, webp64: String) = CryptoCurrenciesEntity.new {
         this.name = name
         this.ticker = ticker
+        this.webp64 = webp64
     }
 
     fun get(ticker: String) = CryptoCurrenciesEntity.find { CryptoCurrenciesTable.ticker eq ticker }.singleOrNull()
